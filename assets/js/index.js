b170aa38723a19bfa096f6b3d792e4fb6c0d94d7
Bayu Satrio Wibowo <bayusatriowid@gmail.com>
2022-10-04 21:53:30 +0700

    feat: add question-form submit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ const questionFormElement = document.getElementById("question-form");
 
 const questionResultElement = document.getElementById("questions-result");
 
+const questionFormElement = document.getElementById("question-form");
+
 const BACKEND_URL =
   "https://api.kontenbase.com/query/api/v1/2fe1b8dd-1b6f-4e91-b884-65382fb84354/Question";
 
@@ -18,7 +20,7 @@ fetchQuestions();
 function displayQuestions(questions) {
   for (const index in questions) {
     let questionElement = `<div>
-    <h1>Usernama: ${questions[index].username}<h1>
+    <h1>Username: ${questions[index].username}<h1>
     <p>Title: ${questions[index].Title}</p>
     <p>Question: ${questions[index].question}</p>
     </div>`;
@@ -27,3 +29,25 @@ function displayQuestions(questions) {
 }
 
 displayQuestions();
+
+const addNewQuestion = async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const newQuestion = {
+    username: formData.get("username")
+    Title: formData.get("question-title"),
+    question: formData.get("question-text"),
+  };
+
+  const response = await fetch(`${BACKEND_API_URL}/questions`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newQuestion),
+  });
+
+  fetchQuestions();
+};
+
+questionFormElement.addEventListener("submit", addNewQuestion);
