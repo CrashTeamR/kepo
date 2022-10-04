258be2fda38801734f1d95801f0331db3c313d55
Denny Setyawan Huda <dennshgit@gmail.com>
2022-10-04 21:24:52 +0700

    feat: add function display questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
-const questionForm = document.getElementById("question-form");
+const questionFormElement = document.getElementById("question-form");
+
+const questionResultElement = document.getElementById("questions-result");
 
 const BACKEND_URL =
   "https://api.kontenbase.com/query/api/v1/2fe1b8dd-1b6f-4e91-b884-65382fb84354/Question";
@@ -8,6 +10,20 @@ async function fetchQuestions() {
   const questions = await response.json();
 
   console.log(questions);
+  displayQuestions(questions);
 }
 
 fetchQuestions();
+
+function displayQuestions(questions) {
+  for (const index in questions) {
+    let questionElement = `<div>
+    <h1>Usernama: ${questions[index].username}<h1>
+    <p>Title: ${questions[index].Title}</p>
+    <p>Question: ${questions[index].question}</p>
+    </div>`;
+    questionResultElement.innerHTML += questionElement;
+  }
+}
+
+displayQuestions();
