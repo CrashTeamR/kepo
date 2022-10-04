fcb5a9d54e7566dbf4d8dea089b3954805ff3158
Bayu Satrio Wibowo <bayusatriowid@gmail.com>
2022-10-04 22:46:57 +0700

    feat: refactor question-form

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,10 @@ const questionFormElement = document.getElementById("question-form");
 const questionResultElement = document.getElementById("questions-result");
 
 const BACKEND_URL =
-  "https://api.kontenbase.com/query/api/v1/2fe1b8dd-1b6f-4e91-b884-65382fb84354/Question";
+  "https://api.kontenbase.com/query/api/v1/2fe1b8dd-1b6f-4e91-b884-65382fb84354";
 
 async function fetchQuestions() {
-  const response = await fetch(`${BACKEND_URL}`);
+  const response = await fetch(`${BACKEND_URL}/Questions`);
   const questions = await response.json();
 
   console.log(questions);
@@ -19,8 +19,8 @@ function displayQuestions(questions) {
   for (const index in questions) {
     let questionElement = `<div>
     <h1>Username: ${questions[index].username}<h1>
-    <p>Title: ${questions[index].Title}</p>
-    <p>Question: ${questions[index].question}</p>
+    <p>Title: ${questions[index].title}</p>
+    <p>Question: ${questions[index].text}</p>
     </div>`;
     questionResultElement.innerHTML += questionElement;
   }
@@ -35,8 +35,8 @@ const addNewQuestion = async (event) => {
 
   const newQuestion = {
     username: formData.get("username"),
-    Title: formData.get("question-title"),
-    question: formData.get("question-text"),
+    title: formData.get("title"),
+    text: formData.get("text"),
   };
 
   const response = await fetch(`${BACKEND_URL}`, {
