f8034f0b37df9bdd09f8f0d5e28b65e61676dd42
ersankarimi <ersankarimii311@gmail.com>
2022-10-08 17:31:04 +0800

    Create index.js
    This file import all module which will used in another file outside modules directory and export it again. This will make it easier to import modules when there are modules to be used outside the modules directory

---
+++ assets/js/modules/index.js
@@ -0,0 +1,5 @@
+import { useState } from "./useState.js";
+import { useFetch } from "./useFetch.js";
+import { displayQuestions } from "./displayQuestions.js";
+
+export { useState, useFetch, displayQuestions };
